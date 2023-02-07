a8ae47fe3f4647cc27def7750f1cfe742887e24b
William-Hortone <willhortone@gmail.com>
2023-02-07 19:17:32 +0800

    added the data

---
+++ src/constants/images.js
@@ -0,0 +1,77 @@
+import logo from "../assets/logo.png";
+import villa1 from "../assets/house1.jpg";
+import villa2 from "../assets/house12.jpg";
+import villa3 from "../assets/villa2.jpg";
+import villa4 from "../assets/house14.jpg";
+import villa5 from "../assets/house4.jpg";
+import villa6 from "../assets/house9.jpg";
+import villa7 from "../assets/house10.jpg";
+import villa8 from "../assets/villa2.jpg";
+import villa9 from "../assets/house16.jpg";
+import villa10 from "../assets/house17.jpg";
+import villa11 from "../assets/house8.jpg";
+import apartment1 from "../assets/villa5.jpg";
+import apartment2 from "../assets/villa1.jpg";
+import apartment3 from "../assets/villa4.jpg";
+import house1 from "../assets/house3.jpg";
+import house2 from "../assets/house13.jpg";
+import btm1 from "../assets/house15.jpg";
+import btm2 from "../assets/house6.jpg";
+import building from "../assets/building.jpg";
+import building1 from "../assets/building1.jpg";
+import building2 from "../assets/building2.jpg";
+import building3 from "../assets/building3.jpg";
+import building4 from "../assets/building4.jpg";
+import building5 from "../assets/building5.jpg";
+import building6 from "../assets/building6.jpg";
+import building7 from "../assets/building7.jpg";
+import plan1 from "../assets/1plan.jpg";
+import plan2 from "../assets/2plan.jpg";
+import plan3 from "../assets/3plan.jpg";
+import plan4 from "../assets/4plan.jpg";
+import plan5 from "../assets/5plan.jpg";
+import service1 from "../assets/service1.jpg";
+import service2 from "../assets/service2.jpg";
+import service3 from "../assets/service3.jpg";
+import boss from "../assets/boss.jpg";
+import team from "../assets/team.jpg";
+
+export default {
+  logo,
+  villa1,
+  villa2,
+  villa3,
+  villa4,
+  villa5,
+  villa6,
+  villa7,
+  villa8,
+  villa9,
+  villa10,
+  villa11,
+  apartment1,
+  apartment2,
+  apartment3,
+  house1,
+  house2,
+  btm1,
+  btm2,
+  building,
+  building1,
+  building2,
+  building3,
+  building4,
+  building5,
+  building6,
+  building7,
+  plan1,
+  plan2,
+  plan3,
+  plan4,
+  plan5,
+  service1,
+  service2,
+  service3,
+  boss,
+  team,
+};
