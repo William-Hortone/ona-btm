90c997b1ce57c2c32b4b405e3a597ac8665dd1d0
William-Hortone <willhortone@gmail.com>
2023-02-14 19:23:42 +0800

    added  navbar

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import "./navbar.css";
-import images from "../../asserts/images";
+import images from "../../constants/images";
 
 const Navbar = () => {
   return (
-    <div className="app__navbar">
-      <div className="app__navbar-logo"></div>
-      <div className="app__navbar-links"></div>
+    <div className="app__navbar ">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo img" />
+      </div>
+      {/* <div className="app__navbar-links"> */}
+      <ul className="app__navbar-links">
+        <li className="navbar__link">
+          <a href="#">Accueil</a>
+        </li>
+        <li className="navbar__link">
+          <a href="#">A propos </a>
+        </li>
+        <li className="navbar__link">
+          <a href="#">Services</a>
+        </li>
+        <li className="navbar__link">
+          <a href="#">Projects</a>
+        </li>
+        <li className="navbar__link">
+          <a href="#">Contact</a>
+        </li>
+      </ul>
     </div>
+    // </div>
   );
 };
 
