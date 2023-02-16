d2e4078ac580ad29f930b9193aed7a9b58e1f1c1
William-Hortone <willhortone@gmail.com>
2023-02-17 01:15:22 +0800

    updated the first service section

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -8,6 +8,7 @@ const Services = () => {
   return (
     <div className="app__service">
       <HeadImage img={images.building4} title="Services" />
+      <ServiceOCA />
       <ServiceOBE />
     </div>
   );
--- src/containers/ServiceOBE/ServiceOBE.jsx
+++ src/containers/ServiceOBE/ServiceOBE.jsx
@@ -8,26 +8,23 @@ const ServiceOBE = () => {
   return (
     <div className="app__serviceOBE">
       <div className="app__serviceOBE-left-box">
-        <HeadText title="Ona conception architecturale" />
-        <p>
-          Les prestations de conception architecturale proposées par ONA
-          CONCEPTION ARCHI s’adaptent aux besoins les plus ciblés de vos
-          chantiers partout dans le Gabon. <br />
-          Dans le cadre d’une remise en état, d’une extension ou d’un projet
-          neuf, nous réalisons :
-        </p>
-        <ul>
-          <li>Esquisses et plans d'aménagement</li>
-          <li>Des plans 3D de construction</li>
-          <li>Des plans de valorisation d’un bâtiment ancien</li>
-          <li>Des plans de surélévation d’appartement</li>
-          <li>Simulation 3D</li>
-        </ul>
+        <CardImage img={images.team} />
       </div>
       <div className="app__serviceOBE-right-box">
-        <div className="serviceOBE-left-box_item s-item1"></div>
-        <div className="serviceOBE-left-box_item s-item2"></div>
-        <div className="serviceOBE-left-box_item s-item3"></div>
+        <HeadText title="Ona bureau d'études : structure" />
+        <p>
+          ONA ÉTUDES est un bureau d’études structure spécialisé dans le calcul,
+          la conception, l’optimisation et les études d’exécutions des
+          structures pour le bâtiment et le génie civil. <br />
+          Alliant rigueur et créativité, nos ingénieurs relèvent les différents
+          challenges techniques des grands projets. Ils analysent et identifient
+          avec précision les contraintes dès la phase d’avant projet tout en
+          conseillant et orientant le client et nos partenaires. Nos ingénieurs
+          jouissent d’une grande expérience et connaissent les multiples genres
+          de structures exploités dans le bâtiment ainsi que les matériaux
+          utilisés, qu’il s’agisse de béton, de métal, de bois ou d’une
+          association entre ces différents matériaux.
+        </p>
       </div>
     </div>
   );
